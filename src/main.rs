ebd20b3433487465e39f798b4a2465628737d73a
Luke Neumann <lfn1112@gmail.com>
2022-12-02 19:02:07 -0700

    day2 and daily modules

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+mod day1;
+mod day2;
+
 fn main() {
-    day1()
+    let args: Vec<String> = std::env::args().collect();
+    let mut day = "1";
+    if args.len() > 1 { day = &args[1]; }
+
+    let content = std::fs::read_to_string(format!("input/{day}.txt")).unwrap();
+    match day {
+        "1" => day1::run(&content),
+        "2" => day2::run(&content),
+        _ => {}
+    }
 }
 
-fn day1() {
-    let content = std::fs::read_to_string("input/1.txt").unwrap();
-    let elves = content.split("\n\n");
-    
-    let mut calorie_sums : Vec<i32> = elves.map(|a| a.split("\n")
-    .map(|a| a.parse::<i32>().unwrap()) // convert to int
-    .sum()).collect();
-    
-    calorie_sums.sort();
-    println!("{:?}", calorie_sums[calorie_sums.len()-1]);
-    println!("{:?}", calorie_sums[calorie_sums.len()-3..].iter().sum::<i32>());
-}
+
+
