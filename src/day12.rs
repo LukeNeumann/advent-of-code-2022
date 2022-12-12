62a16a6e3c63a96768d4530308209157571f3748
Luke Neumann <lfn1112@gmail.com>
2022-12-12 15:08:46 -0700

    day12 p2 speedup

--- src/day12.rs
+++ src/day12.rs
@@ -3,12 +3,28 @@ use petgraph::graph::NodeIndex;
 use petgraph::algo::astar;
 use std::collections::HashMap;
 
-fn add_edge(x1: usize, y1: usize, x2: usize, y2: usize, node_coords: &HashMap::<(usize,usize), NodeIndex>, g: &mut Graph::<char,u32>) {
+fn add_edge(x1: usize, y1: usize, x2: usize, y2: usize, node_coords: &HashMap::<(usize,usize), NodeIndex>, g: &mut Graph::<char,u32>, p2: bool) {
   let index1 = *node_coords.get(&(x1,y1)).expect("no_node1");
   let index2 = *node_coords.get(&(x2,y2)).expect("no_node2");
-  if(g[index1].to_ascii_lowercase() as u32) >= (g[index2].to_ascii_lowercase() as u32 - 1) {
+  let cond = if(p2) {
+    (g[index1].to_ascii_lowercase() as u32) <= (g[index2].to_ascii_lowercase() as u32 + 1)
+  } else {
+    (g[index1].to_ascii_lowercase() as u32) >= (g[index2].to_ascii_lowercase() as u32 - 1)
+  };
+  if cond {
     //println!("{x1} {y1} {:?}; {x2} {y2} {:?};", g[index1], g[index2]);
     g.add_edge(index1, index2, 1);
+  }
+}
+
+fn add_edges(nrows: usize, ncols: usize, node_coords: &HashMap::<(usize,usize), NodeIndex>, g: &mut Graph::<char,u32>, p2: bool) {
+  for y in 0..nrows {
+    for x in 0..ncols {
+      if y > 0       { add_edge(x,y,x,y-1, &node_coords, g, p2); }
+      if y < nrows-1 { add_edge(x,y,x,y+1, &node_coords, g, p2); }
+      if x > 0       { add_edge(x,y,x-1,y, &node_coords, g, p2); }
+      if x < ncols-1 { add_edge(x,y,x+1,y, &node_coords, g, p2); }
+    }
   }
 }
 
@@ -34,26 +50,15 @@ pub fn run(stdout: &mut dyn std::io::Write, content : &str) {
     }
   }
 
-  for (y,line) in lines.iter().enumerate() {
-    for (x,_c) in line.chars().enumerate() {
-      if y > 0             { add_edge(x,y,x,y-1, &node_coords, &mut g); }
-      if y < lines.len()-1 { add_edge(x,y,x,y+1, &node_coords, &mut g); }
-      if x > 0             { add_edge(x,y,x-1,y, &node_coords, &mut g); }
-      if x < line.len()-1  { add_edge(x,y,x+1,y, &node_coords, &mut g); }
-    }
-  }
+  let mut g2 = g.clone();
 
+  add_edges(lines.len(), lines[0].len(), &node_coords, &mut g, false);
   let path = astar(&g, start.expect("s"), |n| n == target.expect("t"), |e| *e.weight(), |_| 0);  
-  let mut min_steps = path.expect("no path!").0;
+  let min_steps = path.expect("no path!").0;
   writeln!(stdout, "{min_steps}").ok();
 
-  for index in g.node_indices().filter(|i| g[*i] == 'a') {
-    let new_path = astar(&g, index, |n| n == target.expect("t"), |e| *e.weight(), |_| 0);
-    let steps = match new_path {
-      Some(path) => path.0,
-      _ => min_steps
-    };
-    if(steps < min_steps) { min_steps = steps; }
-  }
-  writeln!(stdout, "{min_steps}").ok();
+  add_edges(lines.len(), lines[0].len(), &node_coords, &mut g2, true);
+  let path2 = astar(&g2, target.expect("t"), |n| g2[n] == 'a', |e| *e.weight(), |_| 0); 
+  let min_steps2 = path2.expect("no path!").0;
+  writeln!(stdout, "{min_steps2}").ok();
 }
